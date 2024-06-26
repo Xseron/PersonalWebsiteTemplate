8513fbaa2977a86627b9372c045351f30d23639f
David Torossyan <torossyan.david@mail.ru>
2024-06-26 23:28:06 +0500

    Updates

--- _nuxt/DnNsX0p0.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./ARwU3Oho.js";import{y as o,x as t}from"./D4XLuwHP.js";const r={},c={class:"text-3xl font-bold underline"};function n(s,l){return t(),o("h1",c,"Hello world!")}const f=e(r,[["render",n]]);export{f as default};
